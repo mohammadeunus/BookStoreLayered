5be6e586c548563d1a0dcfc1bdfe18c9eecc3044
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-10-31 13:11:50 +0600

    - manually creating author create and update

---
+++ src/Acme.BookStore.Domain/Authors/Author.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using JetBrains.Annotations;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.BookStore.Authors;
+
+public class Author : FullAuditedAggregateRoot<Guid>
+{
+    public string Name { get; private set; }
+    public DateTime BirthDate { get; set; }
+    public string ShortBio { get; set; }
+
+    private Author()
+    {
+        /* This constructor is for deserialization / ORM purpose 
+         * (deserialization meaning : transforming data retrieved from a relational database into object-oriented data structures) */
+    }
+    // Author constructor and ChangeName methods are internal,
+    // so they can be used only in the domain layer
+    internal Author(
+        Guid id,
+        [NotNull] string name,
+        DateTime birthDate,
+        [CanBeNull] string shortBio = null)
+        : base(id)
+    {
+        SetName(name);
+        BirthDate = birthDate;
+        ShortBio = shortBio;
+    }
+
+    internal Author ChangeName([NotNull] string name)
+    {
+        SetName(name);
+        return this;
+    }
+
+    private void SetName([NotNull] string name)
+    {
+        Name = Check.NotNullOrWhiteSpace(
+            name,
+            nameof(name),
+            maxLength: AuthorConsts.MaxNameLength
+        );
+    }
+}
